3e8a2a60a925edb861aae22955bdb3adef3c5f90
Enrico Genauck <enrico.genauck@gateprotect.com>
2015-03-16 15:16:02 +0100

    Changes rails middleware position
    It is now inserted after Rails::Rack::Logger since active record
    is not used in the current test app

--- lib/camel_caser/railtie.rb
+++ lib/camel_caser/railtie.rb
@@ -2,7 +2,7 @@ module CamelCaser
   class Railtie < Rails::Railtie
     initializer 'camel_caser.insert_middleware' do |app|
       # handle request
-      app.config.middleware.insert_after 'ActiveRecord::QueryCache',
+      app.config.middleware.insert_after 'Rails::Rack::Logger',
                                           'CamelCaser::RequestMiddleware'
       # handle response
       app.config.middleware.use 'CamelCaser::ResponseMiddleware'
