6b74d606526f2d629e4ae20098faab13d43de10d
Enrico Genauck <enrico.genauck@gateprotect.com>
2015-03-16 15:21:00 +0100

    fixes deprecation warning in local rails test app

--- spec/integration/apps/rails_app/config/application.rb
+++ spec/integration/apps/rails_app/config/application.rb
@@ -68,5 +68,8 @@ module Dummy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Setting for testing irrelevant secret key base
+    config.secret_key_base = 'secret'
   end
 end
