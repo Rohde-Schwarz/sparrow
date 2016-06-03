c57cc67d7d5e348e3ee6ae44f5601413d031258a
Michael Stark <mstark@gmx.org>
2016-06-03 09:40:18 +0200

    try to include sparrow via path

--- spec/integration/apps/rails_app/config/application.rb
+++ spec/integration/apps/rails_app/config/application.rb
@@ -5,8 +5,8 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
-
-require 'sparrow'
+require File.expand_path("../../../../../../lib/sparrow", __FILE__)
+#require 'sparrow'
 
 Sparrow.configure do |config|
   config.excluded_routes = [
